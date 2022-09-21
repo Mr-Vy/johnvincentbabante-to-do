76b243a5bec910b85e7876251e677be581909653
John Vincent Babante <babantejohnvincent1@gmail.com>
2022-09-21 09:45:23 +0800

    First Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div-container'>
+      <div className="wrapper">
+
+        <Todo />
+
+        <TodoForm 
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+        />
+        
+
+        <TodoList 
+        todos={todos} 
+        setTodos={setTodos}
+        setEditTodo={setEditTodo}
+        />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div className="header">
+      <h1>ToDo List</h1>
+    </div>
+  )
+}
+
+export default Todo
